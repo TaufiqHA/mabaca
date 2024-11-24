1c4a5bff4615ae5ee03e8bb9f8389ae755978d48
taufiq <htaufiq225@gmail.com>
2024-11-24 11:19:11 +0800

    prouduction

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Panel;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
@@ -45,4 +47,18 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        if($panel->getId() === 'admin') {
+            if(Auth::user()) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        return false;
+    }
+
 }
